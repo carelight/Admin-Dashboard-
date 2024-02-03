b5b1354cffd05dd810aa820124033ea33b154ff1
Anshul Upreti <108143494+carelight@users.noreply.github.com>
2024-02-03 17:21:44 +0530

    Update authenticate.js

--- backend/middleware/authenticate.js
+++ backend/middleware/authenticate.js
@@ -3,7 +3,7 @@ const User = require("../models/usermodel");
 const Authenticate =async (req,res,next)=>{
     try{
            const token =req.cookies.jwtoken;
-           const Secret_key ='SASNDKJAHDIHUWDIUAHDIZADHAIDYSZLKJZKSDZDJAWIODIAODAWDOADAKHKSHDKADHAKD';
+           const Secret_key ='Random Alphabets in Capital can be key';
            const verifyToken =jwt.verify(token,Secret_key);
 
            const rootUser = await User.findOne({_id:verifyToken._id,"tokens.token":token});
